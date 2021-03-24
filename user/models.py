b045a44d1902a52ab61cc569289ebbccafbad1c8
Rohan goel <rohangoel501@gmail.com>
2021-03-24 13:33:46 +0530

    First Commit

---
+++ user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class Profile(models.Model):
+	name = models.CharField(max_length=100)
+	email = models.EmailField()
+	image = models.ImageField(upload_to='profile_pics')
+	date_posted = models.DateTimeField(default=timezone.now)
+	
